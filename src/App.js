cdf125ab1eb567ae66c192647f894431a4db35ee
tnaik <98431300+tnaik@users.noreply.github.com>
2024-04-09 14:13:00 -0700

    yay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './Logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header class="bg-white">
+      <nav class="mx-auto flex flex-row max-w-7xl items-center h-full justify-start p-4 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1 gap-4">
+          <img src={logo} alt="BlogIt!" class="w-6 h-6"></img>
+          <h1 class="text-md font-bold">Home</h1>
+          <h1 class="text-md font-bold">Feed</h1>
+        </div>
+      </nav>
+    </header>
   );
 }
 
